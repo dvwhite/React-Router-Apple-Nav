6cc7837c19ebf249f38560ed50de2067a39fdab6
David White <dwhite0627@gmail.com>
2020-01-14 21:55:11 -0500

    Create NavWrapper component

---
+++ apple-nav/src/components/NavWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const NavWrapper = () => {
+  return (
+    <NavLink exact to='/'>Home</NavLink>
+  );
+}
+
+export default NavWrapper;
