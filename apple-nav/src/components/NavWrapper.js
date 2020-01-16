a894c2b6f670b1136fe2bad6a2c06f70b032431d
David White <dwhite0627@gmail.com>
2020-01-15 20:25:23 -0500

    Add NavLink urls to the NavWrapper component

--- apple-nav/src/components/NavWrapper.js
+++ apple-nav/src/components/NavWrapper.js
@@ -10,16 +10,45 @@ const faIconStyle = { color: 'black' };
 const NavWrapper = () => {
   return (
     <>
-      <FontAwesomeIcon icon={faApple} style={faIconStyle} />
-      <NavLink exact to='/mac'>Mac</NavLink>
-      <NavLink exact to='/ipad'>iPad</NavLink>
-      <NavLink exact to='/iphone'>iPhone</NavLink>
-      <NavLink exact to='/watch'>Watch</NavLink>
-      <NavLink exact to='/tv'>TV</NavLink>
-      <NavLink exact to='/music'>Music</NavLink>
-      <NavLink exact to='/support'>Support</NavLink>
-      <FontAwesomeIcon icon={faSearch} style={faIconStyle} />
-      <FontAwesomeIcon icon={faClipboard} style={faIconStyle} />
+      <NavLink to='/'>
+        <FontAwesomeIcon 
+          icon={faApple} 
+          style={faIconStyle} 
+          />
+      </NavLink>
+      <NavLink exact to='/mac'>
+        Mac
+      </NavLink>
+      <NavLink exact to='/ipad'>
+        iPad
+      </NavLink>
+      <NavLink exact to='/iphone'>
+        iPhone
+      </NavLink>
+      <NavLink exact to='/watch'>
+        Watch
+      </NavLink>
+      <NavLink exact to='/tv'>
+        TV
+      </NavLink>
+      <NavLink exact to='/music'>
+        Music
+      </NavLink>
+      <NavLink exact to='/support'>
+        Support
+      </NavLink>
+      <NavLink to='/us/search'>
+        <FontAwesomeIcon 
+          icon={faSearch} 
+          style={faIconStyle} 
+        />
+      </NavLink>
+      <NavLink to='/us/shop'>
+        <FontAwesomeIcon 
+          icon={faClipboard}
+          style={faIconStyle} 
+        />
+      </NavLink>
     </>
   );
 }
